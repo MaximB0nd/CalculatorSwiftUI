99d01c2ea6b26a13ecee081974487c70d246d368
MaximB0nd <sersim11@yandex.ru>
2025-05-03 20:59:25 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Максим Бондарев on 03.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
